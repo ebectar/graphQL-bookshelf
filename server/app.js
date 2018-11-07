93acc75d2d5bca21811d217555b5cc90c9dcc941
ebectar <acebe424@gmail.com>
2018-11-07 11:31:22 -0700

    adding graphiql for querying

--- server/app.js
+++ server/app.js
@@ -1,13 +1,15 @@
-const express = require("express");
-const graphqlHTTP = require("express-graphql");
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 
 const app = express();
 
-app.use("/shelf", graphqlHTTP({
-  schema
+// bind express with graphql
+app.use('/shelf', graphqlHTTP({
+    schema,
+    graphiql: true
 }));
 
 app.listen(4000, () => {
-  console.log("Listening on port 4000!");
+    console.log('Listening on port 4000!');
 });
